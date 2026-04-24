89817aac60ef01c387118c26f016cba45bc8d0b5
Vincent Ngo <vinnyo7@Vincents-MacBook-Air.local>
2026-04-24 15:34:13 -0700

    added LoginForm.jsx, added SignupForm.jsx,added Dashboard.jsx

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+function SignupForm() {
+ return (
+ <form>
+ <input type="text" placeholder="Name" />
+ <input type="email" placeholder="Email" />
+ <input type="password" placeholder="Password" />
+ <button>Signup</button>
+ </form>
+ );
+} export default SignupForm;
